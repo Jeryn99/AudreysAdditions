c906c796404ebc01f32c79d9ad146df99293ba3d
Craig (Jeryn) <sandedshoes@gmail.com>
2025-01-26 16:30:14 +0000

    - Fixed Arm Chair

--- common/src/main/java/dev/jeryn/audreys_additions/blockentity/ChairBlockEntity.java
+++ common/src/main/java/dev/jeryn/audreys_additions/blockentity/ChairBlockEntity.java
@@ -1,10 +1,10 @@
 package dev.jeryn.audreys_additions.blockentity;
 
-import dev.jeryn.audreys_additions.blocks.ChairBaseBlock;
 import dev.jeryn.audreys_additions.common.registry.AudBlockEntities;
 import dev.jeryn.audreys_additions.entity.ChairEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
@@ -12,6 +12,7 @@ import net.minecraft.world.level.block.state.BlockState;
 public class ChairBlockEntity extends BlockEntity {
 
     private ChairEntity chairEntity = null;
+    private DyeColor colour = DyeColor.RED;
 
     public ChairBlockEntity(BlockPos blockPos, BlockState blockState) {
         super(AudBlockEntities.ARMCHAIR.get(), blockPos, blockState);
@@ -29,14 +30,26 @@ public class ChairBlockEntity extends BlockEntity {
         this.chairEntity = chairEntity;
     }
 
+    public DyeColor getColour() {
+        return colour;
+    }
+
+    public void setColour(DyeColor colour) {
+        this.colour = colour;
+        this.setChanged(); // Mark the block entity as changed
+    }
 
     @Override
     public void saveAdditional(CompoundTag tag) {
         super.saveAdditional(tag);
+        tag.putInt("Colour", colour.getId());
     }
 
     @Override
     public void load(CompoundTag tag) {
         super.load(tag);
+        if (tag.contains("Colour")) {
+            this.colour = DyeColor.byId(tag.getInt("Colour"));
+        }
     }
 }
--- fabric/src/main/java/dev/jeryn/audreys_additions/fabric/AudreysAdditionsClientFabric.java
+++ fabric/src/main/java/dev/jeryn/audreys_additions/fabric/AudreysAdditionsClientFabric.java
@@ -2,6 +2,7 @@ package dev.jeryn.audreys_additions.fabric;
 
 import dev.jeryn.audreys_additions.AUDModelRegistry;
 import dev.jeryn.audreys_additions.ClientUtil;
+import dev.jeryn.audreys_additions.blockentity.ChairBlockEntity;
 import dev.jeryn.audreys_additions.client.models.renderers.RenderKnossosChairBlockEntity;
 import dev.jeryn.audreys_additions.client.models.renderers.RenderFoodMachineBlockEntity;
 import dev.jeryn.audreys_additions.common.registry.AudBlockEntities;
@@ -14,7 +15,9 @@ import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.color.block.BlockColor;
 import net.minecraft.client.renderer.entity.NoopRenderer;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
@@ -29,12 +32,17 @@ public class AudreysAdditionsClientFabric implements ClientModInitializer {
         BlockEntityRendererRegistry.register(AudBlockEntities.KNOSSOS_THRONE.get(), RenderKnossosChairBlockEntity::new);
         BlockEntityRendererRegistry.register(AudBlockEntities.FOOD_MACHINE.get(), RenderFoodMachineBlockEntity::new);
 
-        ColorProviderRegistry.BLOCK.register(new BlockColor() {
-            @Override
-            public int getColor(BlockState blockState, @Nullable BlockAndTintGetter blockAndTintGetter, @Nullable BlockPos blockPos, int i) {
-                return Color.RED.getRGB();
+        ColorProviderRegistry.BLOCK.register((blockState, blockAndTintGetter, blockPos, tintIndex) -> {
+            if (blockAndTintGetter != null && blockPos != null) {
+                BlockEntity blockEntity = blockAndTintGetter.getBlockEntity(blockPos);
+                if (blockEntity instanceof ChairBlockEntity chairBlockEntity) {
+                    DyeColor dyeColor = chairBlockEntity.getColour();
+                    return dyeColor.getTextColor();
+                }
             }
+            return DyeColor.WHITE.getTextColor();
         }, AudBlocks.ARMCHAIR.get());
+
 
 
     }
