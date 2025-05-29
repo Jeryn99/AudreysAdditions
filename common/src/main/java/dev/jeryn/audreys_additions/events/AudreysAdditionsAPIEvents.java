de044a530139e2e2608b278978021dab1ac8bbdb
Audrey <alteredbrachacki@gmail.com>
2025-05-29 03:53:42 -0400

    - Added (WIP) Human Nature Console

--- common/src/main/java/dev/jeryn/audreys_additions/events/AudreysAdditionsAPIEvents.java
+++ common/src/main/java/dev/jeryn/audreys_additions/events/AudreysAdditionsAPIEvents.java
@@ -32,6 +32,7 @@ public class AudreysAdditionsAPIEvents {
         TardisClientEvents.CONSOLE_MODELS_SETUP.register((consoleModelCollection, entityModelSet) -> consoleModelCollection.registerModel(AudConsoleRegistry.TOYOTA.getId(), new ConsoleModelEntry(new ToyotaConsoleModel(entityModelSet.bakeLayer(TOYOTA_CONSOLE)))));
         TardisClientEvents.CONSOLE_MODELS_SETUP.register((consoleModelCollection, entityModelSet) -> consoleModelCollection.registerModel(AudConsoleRegistry.KELT.getId(), new ConsoleModelEntry(new KeltConsoleModel(entityModelSet.bakeLayer(KELT_CONSOLE)))));
         TardisClientEvents.CONSOLE_MODELS_SETUP.register((consoleModelCollection, entityModelSet) -> consoleModelCollection.registerModel(AudConsoleRegistry.MCGANN.getId(), new ConsoleModelEntry(new McGannConsoleModel(entityModelSet.bakeLayer(MCGANN_CONSOLE)))));
+        TardisClientEvents.CONSOLE_MODELS_SETUP.register((consoleModelCollection, entityModelSet) -> consoleModelCollection.registerModel(AudConsoleRegistry.HUMAN_NATURE.getId(), new ConsoleModelEntry(new HumanNatureConsoleModel(entityModelSet.bakeLayer(HUMAN_NATURE_CONSOLE)))));
     }
 
 }
