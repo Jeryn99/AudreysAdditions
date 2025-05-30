82ac40462b77a1ddddb488840abe7fd08943b8b5
Craig (Jeryn) <sandedshoes@gmail.com>
2025-05-30 22:43:53 +0100

    - Fixed tools - Fixed Bed dropping twice - Fixed Cat

--- common/src/main/java/dev/jeryn/audreys_additions/mixins/GlobalConsoleBlockEntityMixin.java
+++ common/src/main/java/dev/jeryn/audreys_additions/mixins/GlobalConsoleBlockEntityMixin.java
@@ -1,21 +1,15 @@
 package dev.jeryn.audreys_additions.mixins;
 
 import dev.jeryn.audreys_additions.CatVariantHolder;
-import net.minecraft.core.Holder;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.animal.CatVariant;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import whocraft.tardis_refined.common.blockentity.console.GlobalConsoleBlockEntity;
-
-import java.util.Optional;
 
 @Mixin(GlobalConsoleBlockEntity.class)
 public class GlobalConsoleBlockEntityMixin implements CatVariantHolder {
@@ -33,20 +27,6 @@ public class GlobalConsoleBlockEntityMixin implements CatVariantHolder {
     @Override
     public void setCatVariant(ResourceLocation variant) {
         this.audreysAdditions$catVariantId = variant;
-    }
-
-    @Inject(method = "setConsoleTheme(Lnet/minecraft/resources/ResourceLocation;)V", at = @At("HEAD"), remap = false)
-    private void setConsoleTheme(ResourceLocation themeId, CallbackInfo ci) {
-        GlobalConsoleBlockEntity self = (GlobalConsoleBlockEntity) (Object) this;
-        if(self.theme().getNamespace().contains("human_nature")) {
-            Level level = self.getLevel();
-            if (level instanceof ServerLevel) {
-                Optional<Holder.Reference<CatVariant>> catVariant = BuiltInRegistries.CAT_VARIANT.getRandom(self.getLevel().getRandom());
-                if (self instanceof CatVariantHolder catVariantHolder) {
-                    catVariant.ifPresent(catVariantReference -> catVariantHolder.setCatVariant(catVariantReference.key().location()));
-                }
-            }
-        }
     }
 
     @Inject(method = "saveAdditional", at = @At("TAIL"))
--- forge/src/main/java/dev/jeryn/audreys_additions/forge/data/AudBlockTagsProvider.java
+++ forge/src/main/java/dev/jeryn/audreys_additions/forge/data/AudBlockTagsProvider.java
@@ -1,8 +1,10 @@
 package dev.jeryn.audreys_additions.forge.data;
 
 import dev.jeryn.audreys_additions.AudreysAdditions;
+import dev.jeryn.audreys_additions.common.registry.AudBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -17,6 +19,26 @@ public class AudBlockTagsProvider extends BlockTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider arg) {
+
+        tag(BlockTags.MINEABLE_WITH_AXE)
+                .add(
+                        AudBlocks.KNOSSOS_THRONE.get(),
+                        AudBlocks.ARMCHAIR.get()
+                );
+
+        tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(
+                        AudBlocks.LIMINTON_MONITOR.get(),
+                        AudBlocks.FOOD_MACHINE.get(),
+                        AudBlocks.LIGHTCOLUMN_LEFT.get(),
+                        AudBlocks.LIGHTCOLUMN_RIGHT.get(),
+                        AudBlocks.ASTRAL_MAP.get(),
+                        AudBlocks.CEILING_CANOPY.get(),
+                        AudBlocks.BRACHACKI_MONITOR.get(),
+                        AudBlocks.FOLD_OUT_BED.get()
+                );
+
+
 
     }
 }
