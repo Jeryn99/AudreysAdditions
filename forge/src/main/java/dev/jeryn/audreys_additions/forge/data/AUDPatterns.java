3602a59554a6ee22395773f3b06267e3627cda21
Audrey <alteredbrachacki@gmail.com>
2025-11-27 07:58:54 -0500

    Added Adventure variant to Police Box (1963)

--- forge/src/main/java/dev/jeryn/audreys_additions/forge/data/AUDPatterns.java
+++ forge/src/main/java/dev/jeryn/audreys_additions/forge/data/AUDPatterns.java
@@ -116,8 +116,10 @@ public class AUDPatterns extends ShellPatternProvider {
 
         quickAdd(AudShellRegistry.POLICEBOX_1996.getId(), "default", true);
         quickAdd(AudShellRegistry.POLICEBOX_1996.getId(), "shalka", true);
+
         quickAdd(AudShellRegistry.POLICEBOX_1963.getId(), "default", true);
         quickAdd(AudShellRegistry.POLICEBOX_1963.getId(), "massacre", true);
+        quickAdd(AudShellRegistry.POLICEBOX_1963.getId(), "adventure", true);
 
         quickAdd(AudShellRegistry.SIDRAT.getId(), "sidrat", false, sidratSounds);
         quickAdd(AudShellRegistry.SIDRAT.getId(), "gallifrey", false, sidratSounds);
