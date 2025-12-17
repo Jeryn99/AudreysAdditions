36ffcfa4b287d3263747578919cd212ba61da506
Audrey <alteredbrachacki@gmail.com>
2025-12-16 20:34:50 -0500

    Hatstand progress

--- common/src/main/java/dev/jeryn/audreys_additions/AUDModelRegistry.java
+++ common/src/main/java/dev/jeryn/audreys_additions/AUDModelRegistry.java
@@ -134,13 +134,20 @@ public class AUDModelRegistry {
     public static ModelLayerLocation CEILING_CANOPY = furniture("ceiling_canopy");
     public static ModelLayerLocation CEILING_CANOPY_HANGING = furniture("ceiling_canopy_hanging");
     public static ModelLayerLocation FOLD_OUT_BED = furniture("fold_out_bed");
-    public static ModelLayerLocation HATSTAND_BAMBOO = furniture("hatstand_bamboo");
-    public static ModelLayerLocation HATSTAND_CHERRY = furniture("hatstand_cherry");
-    public static ModelLayerLocation HATSTAND_CRIMSON = furniture("hatstand_crimson");
+    public static ModelLayerLocation CAT = overlords("cat");
+    public static ModelLayerLocation HATSTAND_OAK = furniture("hatstand_oak");
+    public static ModelLayerLocation HATSTAND_SPRUCE = furniture("hatstand_spruce");
+    public static ModelLayerLocation HATSTAND_BIRCH = furniture("hatstand_birch");
+    public static ModelLayerLocation HATSTAND_JUNGLE = furniture("hatstand_jungle");
+    public static ModelLayerLocation HATSTAND_ACACIA = furniture("hatstand_acacia");
     public static ModelLayerLocation HATSTAND_DARK_OAK = furniture("hatstand_dark_oak");
     public static ModelLayerLocation HATSTAND_MANGROVE = furniture("hatstand_mangrove");
-    public static ModelLayerLocation HATSTAND = furniture("hatstand");
-    public static ModelLayerLocation CAT = overlords("cat");
+    public static ModelLayerLocation HATSTAND_CHERRY = furniture("hatstand_cherry");
+    public static ModelLayerLocation HATSTAND_PALE = furniture("hatstand_pale");
+    public static ModelLayerLocation HATSTAND_BAMBOO = furniture("hatstand_bamboo");
+    public static ModelLayerLocation HATSTAND_CRIMSON = furniture("hatstand_crimson");
+    public static ModelLayerLocation HATSTAND_WARPED = furniture("hatstand_warped");
+
 
     public static ModelLayerLocation SPECIMEN_JAR = furniture("specimen_jar/specimen_jar");
     public static ModelLayerLocation SPECIMEN_JAR_ALLAY = furniture("specimen_jar/specimen_jar_allay");
--- common/src/main/java/dev/jeryn/audreys_additions/common/registry/AudBlocks.java
+++ common/src/main/java/dev/jeryn/audreys_additions/common/registry/AudBlocks.java
@@ -99,13 +99,18 @@ public class AudBlocks {
     public static final RegistrySupplier<DoorBlock> ZEITON_QUARTZ_DOOR = register("zeiton_quartz_door",
             () -> new DoorBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR), BlockSetType.ACACIA));
 
-    public static final RegistrySupplier<Block> HATSTAND = register("hatstand", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
-    public static final RegistrySupplier<Block> HATSTAND_BAMBOO = register("hatstand_bamboo", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
-    public static final RegistrySupplier<Block> HATSTAND_CHERRY = register("hatstand_cherry", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
-    public static final RegistrySupplier<Block> HATSTAND_CRIMSON = register("hatstand_crimson", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
+    public static final RegistrySupplier<Block> HATSTAND_OAK = register("hatstand_oak", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
+    public static final RegistrySupplier<Block> HATSTAND_SPRUCE = register("hatstand_spruce", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
+    public static final RegistrySupplier<Block> HATSTAND_BIRCH = register("hatstand_birch", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
+    public static final RegistrySupplier<Block> HATSTAND_JUNGLE = register("hatstand_jungle", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
+    public static final RegistrySupplier<Block> HATSTAND_ACACIA = register("hatstand_acacia", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
     public static final RegistrySupplier<Block> HATSTAND_DARK_OAK = register("hatstand_dark_oak", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
     public static final RegistrySupplier<Block> HATSTAND_MANGROVE = register("hatstand_mangrove", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
-
+    public static final RegistrySupplier<Block> HATSTAND_CHERRY = register("hatstand_cherry", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
+    public static final RegistrySupplier<Block> HATSTAND_PALE = register("hatstand_pale", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
+    public static final RegistrySupplier<Block> HATSTAND_BAMBOO = register("hatstand_bamboo", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
+    public static final RegistrySupplier<Block> HATSTAND_CRIMSON = register("hatstand_crimson", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
+    public static final RegistrySupplier<Block> HATSTAND_WARPED = register("hatstand_warped", () -> new HatStandBlock(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_DOOR)));
     /**
      * Registers a Block and BlockItem to the ItemGroup of your choice
      */
